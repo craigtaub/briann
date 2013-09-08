7b952b1d52a7f9c89fcd0b37bc0a5ec67da38f77
Ubuntu <ubuntu@ip-10-239-81-144.eu-west-1.compute.internal>
2013-09-08 17:47:05 +0000

    comm

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,14 +1,20 @@
+#!/bin/bash
+
+echo 'start'
+
 #JSHINT
-#eval jshint tests/two.js;
+eval jshint tests/two.js;
 
 #PHP CODE SNIFFER
-#eval phpcs tests/one.php;
+eval phpcs tests/one.php;
 
 #COMPILE SASS
-FILE=tests/sass/stylesheets/one.css
+#FILE=tests/sass/stylesheets/one.css
 #rm tests/sass/stylesheets/one.css
 #ls tests/sass/stylesheets/
 if [  -f $FILE ]; then
     eval rm $FILE;
 fi
 eval compass compile tests/sass/;
+
+echo 'executed'
